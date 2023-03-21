5dd1b8b1378ee802a0a214e87ebf9dd9d38c3111
bananahz <hrz8@cornell.edu>
2023-03-21 19:25:24 -0400

    function template

---
+++ backend/booleanSimilarity.py
@@ -0,0 +1,10 @@
+class BooleanSimlarity:
+    docs = []
+
+    def invIndex(good_types, documents):
+        invIndex = {}
+        # for i, enumerate
+
+    def booleanSim(query, good_types):
+        # for q in query
+        print()
