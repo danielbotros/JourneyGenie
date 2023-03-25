5f4ad9bac788a6f09ae952d0ab34bf7380a8c66f
Daniel Botros <dab463@cornell.edu>
2023-03-24 21:50:41 -0400

    pushing size/time functions

--- backend/app.py
+++ backend/app.py
@@ -4,9 +4,9 @@ from flask import Flask, render_template, request
 from flask_cors import CORS
 from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
 
-# ROOT_PATH for linking with all your files. 
+# ROOT_PATH for linking with all your files.
 # Feel free to use a config.py or settings.py with a global export variable
-os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
+os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..", os.curdir))
 
 # These are the DB credentials for your OWN MySQL
 # Don't worry about the deployment credentials, those are fixed
@@ -16,16 +16,18 @@ MYSQL_USER_PASSWORD = "admin123"
 MYSQL_PORT = 3306
 MYSQL_DATABASE = "breeds"
 
-mysql_engine = MySQLDatabaseHandler(MYSQL_USER,MYSQL_USER_PASSWORD,MYSQL_PORT,MYSQL_DATABASE)
+mysql_engine = MySQLDatabaseHandler(
+    MYSQL_USER, MYSQL_USER_PASSWORD, MYSQL_PORT, MYSQL_DATABASE)
 
 # Path to init.sql file. This file can be replaced with your own file for testing on localhost, but do NOT move the init.sql file
-mysql_engine.load_file_into_db(os.path.join(os.environ['ROOT_PATH'],'dogbreeddata.sql'))
+mysql_engine.load_file_into_db(os.path.join(
+    os.environ['ROOT_PATH'], 'dogbreeddata.sql'))
 
 app = Flask(__name__)
 CORS(app)
 
-# # Sample search, the LIKE operator in this case is hard-coded, 
-# # but if you decide to use SQLAlchemy ORM framework, 
+# # Sample search, the LIKE operator in this case is hard-coded,
+# # but if you decide to use SQLAlchemy ORM framework,
 # # there's a much better and cleaner way to do this
 # def sql_search(episode):
 #     query_sql = f"""SELECT * FROM episodes WHERE LOWER( title ) LIKE '%%{episode.lower()}%%' limit 10"""
@@ -33,13 +35,32 @@ CORS(app)
 #     data = mysql_engine.query_selector(query_sql)
 #     return json.dumps([dict(zip(keys,i)) for i in data])
 
+
 @app.route("/")
 def home():
-    return render_template('base.html',title="sample html")
+    return render_template('base.html', title="sample html")
 
 # @app.route("/episodes")
 # def episodes_search():
 #     text = request.args.get("title")
 #     return sql_search(text)
+
+
+def time_commitment(hours):
+    query_sql = f"""SELECT breed_name FROM breeds WHERE 2*(trainability_value + energy_level_value + grooming_frequency_value) <= '%%{hours}%% AND '%%{hours-2}%%' < 2*(trainability_value + energy_level_value + grooming_frequency_value);"""
+    data = mysql_engine.query_selector(query_sql)
+    return data
+
+
+def space_commitment(size):
+    size = size.lower()
+    if size == "small":
+        size = 2
+    elif size == "medium":
+        size = 4
+    else:
+        size = 6
+    return size
+
 
 app.run(debug=True)
