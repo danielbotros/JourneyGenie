0045045fdde16833088730d34812b37852289a94
bananahz <hrz8@cornell.edu>
2023-04-23 22:38:41 -0400

    web scraped images branch changes

--- backend/scraping.py
+++ backend/scraping.py
@@ -3,6 +3,8 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 import sys
+import re
+
 
 
 def processDogBreedInfo():
@@ -13,7 +15,7 @@ def processDogBreedInfo():
   # INITIALIZE CSV
     file = open('export_data.csv', 'w', newline='')
     writer = csv.writer(file)
-    csvheaders = [' Name', 'Description', 'Temperament']
+    csvheaders = [' Name', 'Description', 'Temperament', 'Image', 'Price', 'Hypoallergenic']
     writer.writerow(csvheaders)
     # add each individual item to csv
     file = open('export_data.csv', 'a', newline='', encoding='utf-8')
@@ -55,9 +57,40 @@ def processDogBreedInfo():
 
         dog_breed['description'] = description
         dog_breed['temperament'] = temperament
+
+         # scrapes the urls of the images
+        image_url = "https://www.dogbreedslist.info" + breed_data_soup.find('div', attrs={'class': 'Slides fade'}).find('img')['src']
+        dog_breed['image'] = image_url
+
+        # attempt to download images to the backend/images/ folder
+        # image_data = requests.get(image_url).content
+        # image_file_name = dog_breed['name'].replace(" ", "") + '.jpg'
+        # image_file_path = 'images/' + image_file_name
+        # with open(image_file_path, "wb") as f:
+        #     f.write(image_data)
+        # dog_breed['image'] = image_file_name
+
+        # gets the average prices
+        price = breed_data_soup.find('p', attrs={'class': 'price'}).find_previous_sibling().text
+        dog_breed['price'] = price
+
+        # gets whether or not a dog breed is hypoallergenic
+        try:
+            hypoallergenic = breed_data_soup.find('table', attrs={'class': 'table-02'}).find('p', string = re.compile('Hypoallergenic')).get_text(strip=False)
+        except:
+            try:
+                hypoallergenic = breed_data_soup.find('table', attrs={'class': 'table-02'}).find('td', string = re.compile('Health Issues')).find_next_sibling().find('p').find_next_sibling().find('span').get_text(strip=False)
+            except:
+                try:
+                    hypoallergenic_index = breed_data_soup.find('table', attrs={'class': 'table-02'}).find('td', string = re.compile('Health Issues')).find_next_sibling().text.index('Hypoallergenic')
+                    hypoallergenic = breed_data_soup.find('table', attrs={'class': 'table-02'}).find('td', string = re.compile('Health Issues')).find_next_sibling().text[hypoallergenic_index:]
+                except:
+                    hypoallergenic = "Hypoallergenic: Unknown"
+        dog_breed['hypoallergenic'] = hypoallergenic
+
         file = open('export_data.csv', 'a', newline='', encoding='utf-8')
         writer = csv.writer(file)
-        newheaders = ([finalname, description, temperament])
+        newheaders = ([finalname, description, temperament, image_url, price, hypoallergenic])
         writer.writerow(newheaders)
         dog_breed_info.append(dog_breed)
     return dog_breed_info
